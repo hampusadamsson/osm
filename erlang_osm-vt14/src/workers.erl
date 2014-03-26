f93c0ad0997fee5570739f8ee01ad1220834e295
silverlord86 <kire86@telia.com>
2014-03-27 00:37:57 +0100

    Senaste workers

--- erlang_osm-vt14/src/workers.erl
+++ erlang_osm-vt14/src/workers.erl
@@ -1,30 +1,54 @@
 -module(workers).
 
--export([spawn_worker/4,createProcessList/3,createTestList/0, getResults/0]).
+-export([spawn_worker/3,createProcessList/3,completeTest/1]).
 
-spawn_worker(PID,A,B,specOff) ->
+% spawn_worker(A,B,specOff) ->
 
-	Sum_0 = utils:add_values(A,B,0),
-    Sum_1 = utils:add_values(A,B,1),
-    receive
-    	0 ->
-         	{Result,Y} = Sum_0,
-         	PID ! Y,
-         	Result;
-        1 ->
-           {Result,Y} = Sum_1,
-         	PID ! Y,
-         	Result
-	end;
+% 	Sum_0 = utils:add_values(A,B,0),
+%     Sum_1 = utils:add_values(A,B,1),
+%     receive
+%     	{0,Element,List} ->
+%          	{Result,Carry} = Sum_0,
+%          	if
+% 				Element < length(List) ->
+% 					lists:nth(Element,List) ! {Carry,Element+1,List},
+% 					PID ! Result;
+% 				true ->
+% 					if
+% 						Carry == 0 ->
+% 							PID ! Result;
+% 						true ->
+% 							PID ! [1]++Result
+% 						end
+% 					end;
 
-spawn_worker(PID,A,B,specOn) ->
+			
+%        {1,Element,List} ->
+%            {Result,Carry} = Sum_1,
+%          	if
+% 				Element < length(List) ->
+% 					lists:nth(Element,List) ! {Carry,Element+1,List},
+% 					PID ! Result;
+% 				true ->
+% 					if
+% 						Carry == 0 ->
+% 							PID ! Result;
+% 						true ->
+% 							PID ! [1]++Result
+% 					end
+% 			end	
+		
+			
+% 	end;
+
+spawn_worker(A,B,specOn) ->
 	
 	My_PID = self(),
-	Child_1 = spawn(fun() -> spawn_helper(A,B,0)end),
-	Child_2 = spawn(fun() -> spawn_helper(A,B,1)end),
+	Child_1 = spawn(fun() -> spawn_helper(My_PID,A,B,0)end),
+	Child_2 = spawn(fun() -> spawn_helper(My_PID,A,B,1)end),
 	receive_Loop(Child_1,Child_2,nill,nill).
 
-spawn_helper(A,B,C) ->
+spawn_helper(PID,A,B,C) ->
 	X = utils:add_values(A,B,C),         
     PID ! {X,C}.
 
@@ -36,56 +60,51 @@ receive_Loop(Child_0,Child_1,Carry_0_Result,Carry_1_Result) ->
 		{Result,1} ->
 			receive_Loop(Child_0,Child_1,Carry_0_Result,Result);
 		
-		{0,Element,List} when Carry_0_Result /= nill ->
-			PID ! element(2,Carry_0_Result),
-			exit(Child_1,not_needed),
-			element(1,Carry_0_Result);
+		{0,PID} when Carry_0_Result /= nill ->
+			PID ! Carry_0_Result,
+			exit(Child_1,not_needed);
+			
 
-		{1,Element,List} when Carry_1_Result /= nill ->
-			if
-				Element > length(List) ->
-					body
-			end
-			PID ! element(2,Carry_0_Result),
-			exit(Child_1,not_needed),
-			element(1,Carry_0_Result);
+		{1,PID} when Carry_1_Result /= nill ->
+			PID ! Carry_1_Result,					
+			exit(Child_0,not_needed);
 
 		true ->
 		receive_Loop(Child_0,Child_1,Carry_0_Result,Carry_1_Result)
 
 	end.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 createProcessList(L1,L2,Specc) ->
-	PID = [self()],
+	%PID = [self()],
 	Mode = [Specc],
-	FixedList = [[A,B] ||{A,B} <- lists:zip(L1,L2)].
+	FixedList = [[A,B] ||{A,B} <- lists:zip(L1,L2)],
 	[spawn(fun() -> spawn_worker(B,C,D)end) || [B,C] <- FixedList, D <- Mode].
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-resultTest(X) ->
-	receive 
-		{give_result,PID} ->
-			PID ! X
-	end.
-
-createTestList() ->
-	L1 = ['a','b','c','d','e'],
-	[spawn(fun() -> resultTest(X)end) || X <- L1].
-
-getResults() ->
-	L1 = createTestList(),
-	P = fun(A,AccIn) -> lists:concat([sendMessage(A), AccIn]) end,
-	lists:foldl(P,'',L1).  
+completeTest(Specc) ->
+	L1 = [[8,1],[1,2],[3,3]],
+	L2 = [[1,3],[2,2],[2,3]],
+	L3 = lists:reverse(L1),
+	L4 = lists:reverse(L2),
+ 	ProcessList = createProcessList(L3,L4,Specc),
+ 	{X,Y} = recursiveGet(0,ProcessList,[]),
+ 	if
+ 		Y == 0 ->
+ 			X;
+ 		true ->
+ 			[1]++X
+ 	end.
 
 
-sendMessage(PID) ->
-	OwnPid = self(),
-	PID ! {give_result, OwnPid},
+recursiveGet(Carry,[],SumList) ->
+	{SumList,Carry};
+recursiveGet(Carry,[Head|Tail],SumList) ->
+	My_PID = self(),
+	Head ! {Carry, My_PID},
 	receive
-		X ->
-			X
-		end.
+		{X,Y} ->	
+			recursiveGet(Y,Tail,X++SumList)
+	end.
