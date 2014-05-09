f2f6912464755d2bf397c9bfc9069a785eda3d67
silverlord86 <kire86@telia.com>
2014-05-09 16:16:25 +0200

    Nya GUI:t

---
+++ projekt/GUI/RecvThread.py
@@ -0,0 +1,34 @@
+import threading
+import queue
+import sys
+from threading import Event
+
+class StoppableThread(threading.Thread):
+
+    def __init__(self,serverSocket):
+        super(StoppableThread, self).__init__()
+        self.stopSignal = threading.Event()
+        self.serverSocket = serverSocket.makefile()
+        self.messageQueue = queue.Queue()
+
+    def stop(self):
+        self.stopSignal.set()
+
+    def stopped(self):
+        return self.stopSignal.isSet()
+
+    def run(self):
+         while True:
+            if (self.stopped()):
+                print("Nu sticker tråden med, toodlezzz")
+                sys.exit(0)
+            else:
+                data = self.serverSocket.readline()
+                self.messageQueue.put(data)
+        
+
+    def returnQueue(self):
+        if self.messageQueue.empty():
+            return "empty"
+        else:
+            return self.messageQueue.get()
