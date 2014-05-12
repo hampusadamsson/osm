4bc977d24153979e7257b83c5990db2e9d8ca1d5
KO. Mattsson <karloskar@ponty.se>
2014-05-12 11:10:10 +0200

    Fixed bug

--- projekt/otp_server/server/src/room.erl
+++ projekt/otp_server/server/src/room.erl
@@ -15,7 +15,7 @@
 receivers(Room, List) ->
     case lists:keyfind(Room, 1, List) of
         {Room, Tuple_List}->
-            New_List = lists:foreach(fun({X, _}) -> X end, Tuple_List);
+            New_List = lists:map(fun({X, _}) -> X end, Tuple_List);
         false ->
             New_List = []
     end,
