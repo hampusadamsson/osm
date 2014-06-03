74355b388084e103d74f186c44c2b7a5d85a71de
John Davey <john.davey.edu@gmail.com>
2014-06-03 14:52:31 +0200

    dokumentation storage_handler

--- projekt/src/storage_handler.erl
+++ projekt/src/storage_handler.erl
@@ -11,16 +11,29 @@ empty_rooms([], Ack)->
 empty_rooms([{Name, _, Secrecy}|T],Ack)->
     empty_rooms(T, [{Name,[],Secrecy}|Ack]). 
 
-
+%% ------------------------------------------------------------------
+%% @doc Deletes our saved state
+%% @end 
+%% ------------------------------------------------------------------
 delete_state()->
     {ok, Path} = file:get_cwd(),
     file:delete(Path++"/saved_state").
 
+%% ------------------------------------------------------------------
+%% @doc (MARKED) Saves the state of the room and users to disk
+%%
+%%      State - The State 
+%% @end 
+%% ------------------------------------------------------------------
 save_state(State)->
     {ok, Path} = file:get_cwd(),
     NewState=empty_rooms(State,[]),
     file:write_file(Path++"/saved_state",io_lib:fwrite('~p.',[NewState])).
 
+%% ------------------------------------------------------------------
+%% @doc Recovers the state of rooms and users from disk
+%% @end 
+%% ------------------------------------------------------------------
 recover_state()->
     {ok, Path} = file:get_cwd(),
     case readlines(Path++"/saved_state") of
@@ -32,7 +45,12 @@ recover_state()->
             {error, Reason}
     end.
                        
-            
+%% ------------------------------------------------------------------
+%% @doc Reads a whole file from disk and returns as a string 
+%%
+%%      FileName - The file to read
+%% @end 
+%% ------------------------------------------------------------------
 readlines(FileName) ->
     case  file:open(FileName, [read])  of
         {ok, Device} ->
@@ -44,6 +62,12 @@ readlines(FileName) ->
     {error, Reason}
 end.
 
+%% ------------------------------------------------------------------
+%% @doc Returns all lines in Device as a string.
+%%
+%%      Device - a open file
+%% @end
+%% ------------------------------------------------------------------
 get_all_lines(Device) ->
     case io:get_line(Device, "") of
         eof  -> [];
